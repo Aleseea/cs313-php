fa737122d604fec02155affc0ae7eb4cec1e0089
Aleseea <35313458+Aleseea@users.noreply.github.com>
2018-01-20 19:56:07 -0700

    Add files via upload

---
+++ web/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div>
+        <p>Designed and Developed by Aleseea Pennington</p>
+    </div>
+</footer>
---
+++ web/header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Aleseea's CS 313 Class Site</h1>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="assignments.php">Assignments</a></li>
+        </ul>
+    </nav>
+</header>
