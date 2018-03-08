fd6fa932177542aeb31ec9d53d33e06cff706000
Aleseea <pen14002@byui.edu>
2018-03-07 17:46:25 -0700

    updating the app.js file

--- web/node/myapp/TeamWork/Team09/app.js
+++ web/node/myapp/TeamWork/Team09/app.js
@@ -44,7 +44,8 @@ app.use(function(err, req, res, next) {
 });
 
 
+var port = process.env.PORT || 3000;
 
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.listen(port, () => console.log('Listening on port 8080!'));
 
 module.exports = app;
